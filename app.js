dc86a9989e1d3a33da813c75b47f9f6d81aaab21
王轩 <3003720508@qq.com>
2020-01-13 15:07:43 +0800

    添加node版本

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var  catalogRouter = require('./routes/catalog'); 
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+app.use(helmet());
 
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1/wangxuan';
+const mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/wangxuan';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -25,6 +28,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
